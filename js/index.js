754664400e44ceccf8ca89d8ab14a8eca2a94d78
Alpha-howl <79103525+Alpha-howl@users.noreply.github.com>
2022-06-17 15:15:33 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 
+
+function importAll() {
+    // import needed modules
+
+    const styles = ["css/style.css"];
+
+
+    styles.forEach((styleHref, index) => {
+        const link = document.createElement("link");
+        link.addEventListener("load", () => {
+            moduleLoaded(index);
+        });
+        link.setAttribute("href", styleHref);
+        link.setAttribute("rel", "stylsheet/css");
+        document.head.appendChild(link);
+    });
+
+
+}
+
+
+const loadedModules = [];
+function moduleLoaded(moduleNumber) {
+    console.log("Module", moduleNumber, "has loaded");
+    loadedModules.push(moduleNumber);
+
+    // if all modules loaded, remove loading screen
+}
